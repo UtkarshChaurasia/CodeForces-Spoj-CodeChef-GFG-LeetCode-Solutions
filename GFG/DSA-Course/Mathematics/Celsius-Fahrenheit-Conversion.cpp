dd1065a1c047b913ff05ce1c0ec8c968d9f71704
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-06-16 14:29:19 +0530

    Create Celsius-Fahrenheit-Conversion.cpp

---
+++ GFG/DSA-Course/Mathematics/Celsius-Fahrenheit-Conversion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#define ll long long
+using namespace std;
+int main()
+ {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    cout<<int((n*(1.8)+32))<<endl;
+	}
+	return 0;
+}
