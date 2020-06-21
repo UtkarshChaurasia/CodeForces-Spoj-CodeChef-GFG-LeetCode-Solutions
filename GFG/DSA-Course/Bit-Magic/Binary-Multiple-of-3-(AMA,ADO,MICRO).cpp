25bee44b8b23ece18e8d4effdd964ad72ed7dbc7
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-06-21 14:29:20 +0530

    Create Binary-Multiple-of-3-(AMA,ADO,MICRO).cpp

---
+++ GFG/DSA-Course/Bit-Magic/Binary-Multiple-of-3-(AMA,ADO,MICRO).cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define pb push_back
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+	ll num=0,countodd=0,counteven=0;
+	string str;
+	cin>>str;
+	for(int i=0;i<str.length();i++)
+	{
+	    if(str[i]=='1' && (i%2==0))
+	    {
+	        counteven++;
+	    }
+	    else if(str[i]=='1' && (i%2)!=0)
+	    {
+	        countodd++;
+	    }
+	}
+	//deb(counteven);
+	//deb(countodd);
+	if((abs(counteven-countodd))%3==0)
+	{
+	    cout<<"1"<<endl;
+	}
+	else
+	{
+	    cout<<"0"<<endl;
+	}
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
