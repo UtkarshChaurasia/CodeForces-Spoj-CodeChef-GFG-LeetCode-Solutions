283cc9b04bd58f83da7032b92a1fbd00dde58e7d
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-06-22 15:52:33 +0530

    Create Swap-every-two-bits-in-bytes(CISCO).cpp

---
+++ GFG/DSA-Course/Bit-Magic/Swap-every-two-bits-in-bytes(CISCO).cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) a.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define pb push_back
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,b,rem,count=0,sum=0,temp;
+	cin>>n;
+	bitset<8>a(n);
+	fo(i,7)
+	{
+	    temp = a[i];
+	    a[i] = a[i+1];
+	    a[i+1] = temp;
+	    i++;
+	}
+	cout<<a.to_ulong()<<endl;
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
