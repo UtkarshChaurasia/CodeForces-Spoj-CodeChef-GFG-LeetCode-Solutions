c409c1bf3725854487241ba816bee9afc04228e8
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-06-24 00:19:24 +0530

    Optimized sol in first try

---
+++ GFG/DSA-Course/Bit-Magic/Copy-Set-Bits-in-Range-(DE-SHAW).cpp
@@ -0,0 +1,94 @@
+/*Best optimized solution. Got this correct in first try
+  Time: O(32) depends on (l-r)
+  Space: O(1)
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) a.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define pb push_back
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0,l,r,x,y,AND=0,result,copyy;
+	cin>>x>>l>>r>>y;
+	copyy = y;
+	while(copyy!=0)
+	{
+	    count++;
+	    copyy = copyy>>1;
+	}
+	if(r>count)
+	{
+	    r = count;
+	}
+	for(i = l;i<=r;i++)
+	{
+	    AND = AND+pow(2,(i-1));
+	}
+	result = (y & AND) | x;
+	cout<<result<<endl;
+	
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
