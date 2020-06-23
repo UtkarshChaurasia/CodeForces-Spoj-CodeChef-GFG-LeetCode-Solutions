a84075dd4ae74f230a0adb7bf7133abe6d2a0a83
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-06-23 18:24:36 +0530

    Create Flip-bits-max1s-(KADANE)-(AMAZON).cpp

---
+++ GFG/DSA-Course/Bit-Magic/Flip-bits-max1s-(KADANE)-(AMAZON).cpp
@@ -0,0 +1,98 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) a.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define pb push_back
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,max_sum=0,sum_now=0;
+	cin>>n;
+	int arr[n];
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	    if(arr[i]==1)
+	    {
+	        count++;
+	        arr[i] = -1;
+	    }
+	    else
+	    {
+	        arr[i] = 1;
+	    }
+	}
+	fo(i,n)
+	{
+	    sum_now += arr[i];
+	    if(sum_now<0)
+	    {
+	        sum_now=0;
+	    }
+	    if(sum_now>max_sum)
+	    {
+	        max_sum=sum_now;
+	    }
+	}
+	cout<<(count+max_sum)<<endl;
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
