b57f7260a79f83f1ff06b54280b530016c5f7451
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-03 14:46:37 +0530

    Create Sorting-by-count-1s.cpp

---
+++ Leetcode/Easy/Bit-Manipulation/Sorting-by-count-1s.cpp
@@ -0,0 +1,36 @@
+/*Given an integer array arr. You have to sort the integers in the array in ascending order by the number of 1's in their binary representation and in case of two or more integers have the same number of 1's you have to sort them in ascending order.
+
+Return the sorted array.
+
+Example 1:
+
+Input: arr = [0,1,2,3,4,5,6,7,8]
+Output: [0,1,2,4,8,3,5,6,7]
+Explantion: [0] is the only integer with 0 bits.
+[1,2,4,8] all have 1 bit.
+[3,5,6] have 2 bits.
+[7] has 3 bits.
+The sorted array by bits is [0,1,2,4,8,3,5,6,7]
+
+Example 2:
+
+Input: arr = [1024,512,256,128,64,32,16,8,4,2,1]
+Output: [1,2,4,8,16,32,64,128,256,512,1024]
+Explantion: All integers have 1 bit in the binary representation, you should just sort them in ascending order.
+*/
+
+class Solution {
+public:
+    
+    static bool compare(const int& a, const int& b){
+        int c1 = __builtin_popcount(a);
+        int c2 = __builtin_popcount(b);
+        if(c1 == c2)
+            return a < b;
+        return c1 < c2;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        std::sort(arr.begin(),arr.end(), compare);
+        return arr;
+    }
+}; 
