a9142f846a0cf6c2958b88edaeb4eba652c29e20
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-19 00:32:53 +0530

    Create Max_diff_element(MORGAN,SAP,MMT,OLA).cpp

---
+++ GFG/DSA-Course/Arrays/Max_diff_element(MORGAN,SAP,MMT,OLA).cpp
@@ -0,0 +1,127 @@
+/*
+You are given an array A (distinct elements) of size N. Find out the maximum difference between any two elements such that larger element appears after the smaller number in A.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. T testcases follow. Each testcase contains two lines of input. The first line of each test case is N,N is the size of array. The second line of each test case contains N elements separated by spaces.
+
+Output:
+For each testcase, print the maximum difference between two element. Otherwise print -1
+
+Constraints:
+1 <= T <= 100
+2 <= N <= 107
+0 <= Ai <= 107
+
+Example:
+Input:
+2
+7
+2 3 10 6 4 8 1
+6
+7 9 5 6 3 2
+
+Output:
+8
+2
+
+Optimized:
+Time: O(N)
+Space: O(1)
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n-1;i>=0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+	cin>>n;
+	vi arr(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	int max_dif = arr[1]-arr[0];
+	int min_ele = arr[0];
+	dfo(1,n,1)
+	{
+	    if((arr[i]-min_ele)>max_dif)
+	    {
+	        max_dif = arr[i]-min_ele;
+	    }
+	    if(arr[i]<min_ele)
+	    {
+	        min_ele = arr[i];
+	    }
+	}
+	cout<<max_dif<<endl;
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
