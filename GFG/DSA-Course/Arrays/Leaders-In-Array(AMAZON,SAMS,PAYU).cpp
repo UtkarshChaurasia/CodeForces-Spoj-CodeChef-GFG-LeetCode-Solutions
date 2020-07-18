ddd6ba1b5729623bbd43198b0c5f56b0fd9a5c2f
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-18 22:58:45 +0530

    Create Leaders-In-Array(AMAZON,SAMS,PAYU).cpp

---
+++ GFG/DSA-Course/Arrays/Leaders-In-Array(AMAZON,SAMS,PAYU).cpp
@@ -0,0 +1,134 @@
+/*Given an array of positive integers. Your task is to find the leaders in the array.
+Note: An element of array is leader if it is greater than or equal to all the elements to its right side. Also, the rightmost element is always a leader. 
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
+The first line of each test case contains a single integer N denoting the size of array.
+The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+Print all the leaders.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 107
+0 <= Ai <= 107
+
+Example:
+Input:
+3
+6
+16 17 4 3 5 2
+5
+1 2 3 4 0
+5
+7 4 5 7 3
+Output:
+17 5 2
+4 0
+7 7 3
+
+Optimized:
+Time O(n)
+
+*/
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n-1;i>=0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+	cin>>n;
+	vi arr(n);
+	vi res;
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	int curr_lead = arr[n-1];
+	res.pb(arr[n-1]);
+	forev(i,n-1)
+	{
+	    if(curr_lead<=arr[i])
+	    {
+	        curr_lead = arr[i];
+	        res.pb(curr_lead);
+	    }
+	}
+	forev(i,res.size())
+	{
+	    cout<<res[i]<<" ";
+	}
+	cout<<endl;
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
