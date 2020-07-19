80cdda7bd5e3ed07be15dc8044208a897bc18aac
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-20 00:53:59 +0530

    Create Trapping-Rainwater(AMAZON,ADOBE,PAYTM,ACCOLITE,PAYU).cpp

---
+++ GFG/DSA-Course/Arrays/Trapping-Rainwater(AMAZON,ADOBE,PAYTM,ACCOLITE,PAYU).cpp
@@ -0,0 +1,160 @@
+/*
+Given an array arr[] of N non-negative integers representing height of blocks at index i as Ai where the width of each block is 1. Compute how much water can be trapped in between blocks after raining.
+Structure is like below:
+|  |
+|_|
+We can trap 2 units of water in the middle gap.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. Each test case contains an integer N denoting the size of the array, followed by N space separated numbers to be stored in array.
+
+Output:
+Output the total unit of water trapped in between the blocks.
+
+Constraints:
+1 <= T <= 100
+3 <= N <= 107
+0 <= Ai <= 108
+
+Example:
+Input:
+2
+4
+7 4 0 9
+3
+6 9 9
+
+Output:
+10
+0
+
+Explanation:
+Testcase 1: Water trapped by block of height 4 is 3 units, block of height 0 is 7 units. So, total unit of water trapped is 10 units.
+
+OPTIMIZED
+Time: O(N)
+Space: O(N)
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n-1;i>=0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0,res = 0;
+	cin>>n;
+	vi arr(n);
+	vi rmax(n);
+	vi lmax(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	
+	//generating an array of left max value for each index
+	
+	lmax[0] = arr[0];
+	dfo(1,n,1)
+	{
+	    if(arr[i]>lmax[i-1])
+	    {
+	        lmax[i] = arr[i];
+	    }
+	    else
+	    {
+	        lmax[i] = lmax[i-1];
+	    }
+	}
+	
+	//generating an array of right max value for each index
+	
+	rmax[n-1] = arr[n-1];
+	forev(i,n-1)
+	{
+	    if(arr[i]>=rmax[i+1])
+	    {
+	        rmax[i] = arr[i];
+	    }
+	    else
+	    {
+	        rmax[i] = rmax[i+1];
+	    }
+	}
+	
+	fo(i,n)
+	{
+	    res = res + (min(rmax[i],lmax[i]) - arr[i]);
+	}
+	cout<<res<<endl;
+	
+    
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
