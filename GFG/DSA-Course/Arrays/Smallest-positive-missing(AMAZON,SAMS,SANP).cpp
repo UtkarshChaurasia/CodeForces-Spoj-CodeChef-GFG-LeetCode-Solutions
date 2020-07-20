1da60fc163ab1280a3a378238a0bfe2c7fd8a497
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-20 23:27:21 +0530

    Create Smallest-positive-missing(AMAZON,SAMS,SANP).cpp

---
+++ GFG/DSA-Course/Arrays/Smallest-positive-missing(AMAZON,SAMS,SANP).cpp
@@ -0,0 +1,131 @@
+/*
+You are given an array arr[] of N integers including 0. The task is to find the smallest positive number missing from the array.
+Note: Expected solution in O(n) time using constant extra space.
+
+Input:
+First line consists of T test cases. First line of every test case consists of N, denoting the number of elements in array. Second line of every test case consists of elements in array.
+
+Output:
+Single line output, print the smallest positive number missing.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 106
+-106 <= arr[i] <= 106
+
+Example:
+Input:
+2
+5
+1 2 3 4 5
+5
+0 -10 1 3 -20
+Output:
+6
+2
+
+TIME: O(N)
+SPACE: O(N)
+
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0,ans=0;
+	cin>>n;
+	vi arr(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	bool present[n+1] = {false};
+	fo(i,n)
+	{
+	    if(arr[i]>0 && arr[i]<=n)
+	    {
+	        present[arr[i]] = true;
+	    }
+	}
+	dfo(1,n+1,1)
+	{
+	    if(present[i] == false)
+	    {
+	        ans = i;
+	        break;
+	    }
+	}
+	cout<<ans<<endl;
+
+
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
