c8acf63c584b58489b72448a3ff26ad6a5338607
UtkarshChaurasia <msdianuc07@gmail.com>
2020-07-21 23:56:06 +0530

    Update

---
+++ Codeforces/Contest/#658/B-Sequential-Nim.cpp
@@ -0,0 +1,120 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n-1;i>=0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0,f=0,s=0;
+    cin>>n;
+    vi arr(n);
+    fo(i,n)
+    {
+        cin>>arr[i];
+    }
+    fo(i,n)
+    {
+        if(arr[i]!=1)
+        {
+            break;
+        }
+        else if(arr[i]==1)
+        {
+            count++;
+        }
+
+    }
+    if(count==0)
+    {
+        cout<<"First"<<endl;
+    }
+    else if(count%2 == 0)
+    {
+        if(count==n)
+        {
+            cout<<"Second"<<endl;
+        }
+        else
+        {
+            cout<<"First"<<endl;
+        }
+        
+    }
+    else
+    {
+        if(count==n)
+        {
+            cout<<"First"<<endl;
+        }
+        else
+        {
+            cout<<"Second"<<endl;
+        }
+    }
+    
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
