7bdedd42b6c97513d03b6dcc27fc3ce903cbe4af
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-30 23:56:58 +0530

    Create Freq-of-Limited-Range-Array(AMAZON,PAYTM,VMWARE,IMP).cpp

---
+++ GFG/DSA-Course/Arrays/Freq-of-Limited-Range-Array(AMAZON,PAYTM,VMWARE,IMP).cpp
@@ -0,0 +1,144 @@
+/*
+Given an array A[] of N positive integers which can contain integers from 1 to N where elements can be repeated or can be absent from the array. Your task is to count frequency of all elements from 1 to N.
+
+Note: Expected time complexity is O(n) with O(1) extra space.
+
+Input:
+First line of input contains an integer T denoting the number of test cases. For each test case, first line contains an integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+For each test case, output N space-separated integers denoting the frequency of each element from 1 to N.
+
+Constraints:
+1 ≤ T ≤ 100
+1 ≤ N ≤ 106
+1 <= A[i] <= 106
+
+Example:
+Input:
+2
+5
+2 3 2 3 5
+4
+3 3 3 3
+
+Output:
+0 2 2 0 1
+0 0 4 0
+
+Explanation:
+Testcase 1: Counting frequencies of each array elements, we have:
+1 occurring 0 times.
+2 occurring 2 times.
+3 occurring 2 times.
+4 occurring 0 times.
+5 occurring 1 time.
+
+OPTIMIZED
+
+TIME: O(N)
+SPACE: O(1)
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+	cin>>n;
+	vi arr(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	// decreasing every element by 1
+	
+	fo(i,n)
+	{
+	    arr[i]--;
+	}
+	 //adding n to elements according to their frequency
+	 fo(i,n)
+	 {
+	     
+	     arr[arr[i]%n] = arr[arr[i]%n] + n;
+	     
+	 }
+	 
+	 //finding the frequency and printing
+	 fo(i,n)
+	 {
+	     arr[i] = arr[i]/n;
+	     cout<<arr[i]<<" ";
+	 }
+	 cout<<endl;
+	
+}
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
