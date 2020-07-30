619b04b1fffc25923a0527fb81ed4c84947b495f
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-07-31 00:25:20 +0530

    Create Equilibrium-Point(AMAZON,ADOBE,ZOHO,ACCOLITE).cpp

---
+++ GFG/DSA-Course/Arrays/Equilibrium-Point(AMAZON,ADOBE,ZOHO,ACCOLITE).cpp
@@ -0,0 +1,133 @@
+/*
+Given an array A of N positive numbers. The task is to find the position where equilibrium first occurs in the array. Equilibrium position in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Input:
+The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow. First line of each test case contains an integer N denoting the size of the array. Then in the next line are N space separated values of the array A.
+
+Output:
+For each test case in a new  line print the position at which the elements are at equilibrium if no equilibrium point exists print -1.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 106
+1 <= Ai <= 108
+
+Example:
+Input:
+2
+1
+1
+5
+1 3 5 2 2
+
+Output:
+1
+3
+
+Explanation:
+Testcase 1: Since its the only element hence its the only equilibrium point.
+Testcase 2: For second test case equilibrium point is at position 3 as elements below it (1+3) = elements after it (2+2).
+
+OPTIMIZED
+
+TIME: O(N)
+SPACE: O(1)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sumend=0, sumbeg=0;
+	cin>>n;
+	vi arr(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	    sumend += arr[i]; 
+	}
+	fo(i,n)
+	{
+	    sumend -= arr[i];
+	    if(sumbeg == sumend)
+	    {
+	        cout<<(i+1)<<endl;
+	        break;
+	    }
+	    if(i==(n-1))
+	    {
+	        cout<<-1<<endl;
+	    }
+	    sumbeg += arr[i];
+	    
+	}
+	
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
