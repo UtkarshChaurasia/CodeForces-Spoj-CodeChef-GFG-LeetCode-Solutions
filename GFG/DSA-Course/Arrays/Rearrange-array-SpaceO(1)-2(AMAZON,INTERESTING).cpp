f1f14ffbdc332053d72695c00c980adf5500a881
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-08-01 20:00:10 +0530

    Create Rearrange-array-SpaceO(1)-2(AMAZON,INTERESTING).cpp

---
+++ GFG/DSA-Course/Arrays/Rearrange-array-SpaceO(1)-2(AMAZON,INTERESTING).cpp
@@ -0,0 +1,130 @@
+/*
+Given an array arr[] of size N where every element is in range from 0 to n-1. Rearrange the given array so that arr[i] becomes arr[arr[i]]. This should be done with O(1) extra space.
+
+Input:
+First line contains an integer denoting the test cases 'T'. First line of each test case contains an integer value depicting size of array 'N' and next line contains N space separated integers denoting the elements of the array.
+
+Output:
+Print all elements of the array after rearranging, each separated by a space, in separate line for each test case.
+
+Constraints:
+1 <= T <= 100
+1 <= N<= 107
+0 <= Arr[i] < N
+
+Example:
+Input:
+3
+2
+1 0
+5
+4 0 2 1 3
+4
+3 2 0 1
+
+Output:
+0 1
+3 4 2 0 1
+1 0 3 2
+
+Explanation:
+Testcase 1: arr[0] = 1 and arr[arr[0]] = 0. Also, arr[1] = 0 and arr[arr[1]] = 1. So, rearranging elements, we get array as, 0 1.
+
+OPTIMIZED
+
+TIME: O(N)
+SPACE: O(N)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+void solve()
+{
+	ll i,j,n,m,a,b,rem,count=0,sum=0;
+	cin>>n;
+	vl arr(n);
+	fo(i,n)
+	{
+	    cin>>arr[i];
+	}
+	ll  max = *max_element(arr.begin(),arr.end()) + 1;
+	fo(i,n)
+	{
+	    arr[i] += (arr[arr[i]] % max)*max;
+	}
+	fo(i,n)
+	{
+	    arr[i] = arr[i]/max;
+	    cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+	
+}
+
+
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
