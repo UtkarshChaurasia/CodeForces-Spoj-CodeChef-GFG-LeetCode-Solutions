d019d2253e2ff294500badf51efaed8a4b90ff78
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-09-23 17:24:39 +0530

    Update Quick-Sort_(Lomuto-Partition).cpp

--- GFG/DSA-Course/Sorting/Quick-Sort_(Lomuto-Partition).cpp
+++ GFG/DSA-Course/Sorting/Quick-Sort_(Lomuto-Partition).cpp
@@ -19,7 +19,7 @@ SPACE: O(1)
 #include <bits/stdc++.h>
 using namespace std;
 
-int partition (int arr[], int low, int high);
+int lomutoPartition (int arr[], int low, int high);
 /* The main function that implements QuickSort
  arr[] --> Array to be sorted, low  --> Starting index, high  --> Ending index */
  
@@ -28,7 +28,7 @@ void quickSort(int arr[], int low, int high)
     if (low < high)
     {
         /* pi is partitioning index, arr[p] is now at right place */
-        int pi = partition(arr, low, high);
+        int pi = lomutoPartition(arr, low, high);
         // Separately sort elements before partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
@@ -61,7 +61,7 @@ int main()
 /* This function takes last element as pivot, places  the pivot element 
    at its correct position in sorted  array, and places all smaller (smaller
    than pivot) to left of pivot and all greater elements to right  of pivot */
-int partition (int arr[], int low, int high)
+int lomutoPartition (int arr[], int low, int high)
 {
    // Your code here
    int pivot = arr[high];
