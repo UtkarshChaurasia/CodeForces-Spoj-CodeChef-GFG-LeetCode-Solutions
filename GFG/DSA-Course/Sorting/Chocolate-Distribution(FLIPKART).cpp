979fb40ce43c1f6a6353b35de635066d209c7947
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-09-26 19:34:48 +0530

    Update Chocolate-Distribution(FLIPKART).cpp

--- GFG/DSA-Course/Sorting/Chocolate-Distribution(FLIPKART).cpp
+++ GFG/DSA-Course/Sorting/Chocolate-Distribution(FLIPKART).cpp
@@ -1,4 +1,5 @@
-/*Given an array A of positive integers of size N, where each value represents number of chocolates in a packet. Each packet can have variable number of chocolates.
+/*Given an array A of positive integers of size N, where each value represents number of chocolates in a packet. Each packet can have variable number 
+of chocolates.
 There are M students, the task is to distribute chocolate packets such that :
 1. Each student gets one packet.
 2. The difference between the number of chocolates given to the students having packet with maximum chocolates and student having packet with minimum chocolates 
