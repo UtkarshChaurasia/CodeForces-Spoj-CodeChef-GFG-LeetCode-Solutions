1e406f0e3c8926f0b124d779290dbfbf8319e2d5
Utkarsh Chaurasia <msdianuc07@gmail.com>
2020-12-08 20:11:45 +0530

    Create Frequency-Sorting(ORACLE,ZYCUS).cpp

---
+++ GFG/DSA-Course/Hashing/Frequency-Sorting(ORACLE,ZYCUS).cpp
@@ -0,0 +1,145 @@
+/*
+Given an array A[] of integers, sort the array according to frequency of elements. That is elements that have higher frequency come first. If frequencies
+of two elements are same, then smaller number comes first.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows. The first line of each test case
+contains a single integer N denoting the size of array. The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+
+Output:
+For each testcase, in a new line, print each sorted array in a seperate line. For each array its numbers should be seperated by space.
+
+Constraints:
+1 ≤ T ≤ 70
+30 ≤ N ≤ 130
+1 ≤ Ai ≤ 60 
+
+Example:
+Input:
+2
+5
+5 5 4 6 4
+5
+9 9 9 2 5
+
+Output:
+4 4 5 5 6
+9 9 9 2 5
+
+Explanation:
+Testcase1: The highest frequency here is 2. Both 5 and 4 have that frequency. Now since the frequencies are same then smaller element comes first. So 4 4 
+comes first then comes 5 5. Finally comes 6.
+The output is 4 4 5 5 6.
+
+Testcase2: The highest frequency here is 3. The element 9 has the highest frequency. So 9 9 9 comes first. Now both 2 and 5 have same frequency. So we 
+print smaller element first.
+The output is 9 9 9 2 5.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define fo2(j,n) for(j=0;j<n;j++)
+#define forev(i,n) for(i=n;i>0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+
+bool comp(pii a, pii b)
+{
+    if(a.S==b.S)
+    {
+        return a.F<b.F;
+    }
+    return a.S>b.S;
+}
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+	cin>>n;
+	unordered_map<int,int> mp;
+	fo(i,n)
+	{
+	    int ele;
+	    cin>>ele;
+	    mp[ele]++;
+	}
+	vector<pii> ans;
+	for(auto it : mp)
+	{
+	    ans.pb(make_pair(it.F,it.S));
+	}
+	sort(ans.begin(),ans.end(),comp);
+	
+	fo(i,ans.size())
+	{
+	    while(ans[i].S>0)
+	    {
+	        cout<<ans[i].F<<" ";
+	        ans[i].S--;
+	    }
+	}
+	cout<<endl;
+	
+}
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
