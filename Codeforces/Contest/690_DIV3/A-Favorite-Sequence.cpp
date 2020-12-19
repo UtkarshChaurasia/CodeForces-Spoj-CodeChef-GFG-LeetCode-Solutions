e29259f8b6698c646474392efa7c68a854169f2a
UtkarshChaurasia <msdianuc07@gmail.com>
2020-12-19 14:52:15 +0530

    folders renamed

---
+++ Codeforces/Contest/690_DIV3/A-Favorite-Sequence.cpp
@@ -0,0 +1,154 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(i=0;i<n;i++)
+#define forev(i,n) for(i=n-1;i>=0;i--)
+#define dfo(a,b,c) for(i=a;i<b;i+=c)
+#define si(x)  scanf("%d", &x)
+#define sl(x)  scanf("%lld", &x)
+#define ss(s)  sacnf("%s", s)
+#define pi(x)  printf("%d\n", x)
+#define pl(x)  printf("%lld\n", x)
+#define ps(s)  printf("%s\n", s)
+#define deb(x) cout<< #x << "=" << x << endl;
+#define deb2(x, y) cout<< #x << "=" << x <<","<< #y << "=" << y <<endl;
+#define all(x) x.begin(), x.end()
+#define sortall(x) sort(all(x))
+#define find(x,n) binary_search(all(x), n)
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll>   pl; 
+typedef vector<int>   vi;
+typedef vector<ll>    vl;
+typedef vector<vi>    vvi;
+typedef vector<vl>    vvl;
+typedef set<int>   si;
+typedef set<ll>   sl;
+typedef unordered_map<int, int> umi;
+typedef map<int, int> mi;
+
+const int MOD = 1000000007;   //(10^9 + 7)
+
+
+bool isprime(ll n) 
+{ 
+  if (n <= 1) return false; 
+  if (n <= 3) return true; 
+
+  if (n % 2 == 0 || n % 3 == 0) return false; 
+
+  for (ll i = 5; i * i <= n; i += 6) 
+	if (n % i == 0 || n % (i+2) == 0) 
+	  return false; 
+
+  return true; 
+} 
+
+bool prime[15000105]; 
+void sieve(int n) 
+{ 
+  for (ll i = 0; i <= n; i++) prime[i] = 1;
+  for (ll p = 2; p * p <= n; p++) 
+  { 
+	if (prime[p] == true) 
+	{ 
+	  for (ll i = p * p; i <= n; i += p) 
+		prime[i] = false; 
+	} 
+  } 
+  prime[1] = prime[0] = 0;
+}
+
+
+
+ll gcdll(ll a,ll b) {
+    if (b==0) return a;
+    return gcdll(b,a%b);
+}
+
+
+int gcd(int a,int b) {
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+
+
+int mpow(int base, int exp)
+{
+	base %= MOD;
+	int result = 1;
+	while(exp>0)
+	{
+		if(exp & 1)
+		{
+			result = ((ll)result*base);
+		}
+		
+		base = ((ll)base*base) % MOD;
+		exp >>= 1;
+	}
+	
+	return result;
+}
+
+
+
+void solve()
+{
+	int i,j,n,m,a,b,rem,count=0,sum=0;
+    cin>>n;
+    vi arr(n);
+    vi res;
+    fo(i,n)
+    {
+        cin>>arr[i];
+    }
+    if(n==1)
+    {
+        cout<<arr[0];
+    }
+    else
+    {
+        
+    
+    
+    i = 0;
+    j = n-1;
+    while(i<=j)
+    {
+        res.pb(arr[i]);
+        if(i!=j)
+        {res.pb(arr[j]);}
+        
+        i++;
+        j--;
+
+    }
+    fo(i,res.size())
+    {
+        cout<<res[i]<<" ";
+    }
+    }
+    cout<<endl;
+}
+
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		solve();
+	}
+	
+	return 0;
+}
+
+
---
+++ Codeforces/Contest/690_DIV3/README.md
@@ -0,0 +1,33 @@
+# [690 - DIV 3](https://codeforces.com/contest/1462)
+
+## Problem A. [QUESTION](https://codeforces.com/contest/14562/problem/A)
+
+- Simple problem. Two pointer approach.
+- Typing took a bit of time<br>
+Learning: **A problems are always fun xD**.
+
+## Problem B. [QUESTION](https://codeforces.com/contest/1462/problem/B)
+
+- Very simple problem even easier than A.
+- Can be easily solved by hard coding if else cases.
+- Overthought a lot which huge penalty of time.<br>
+Learning: **First try brute force approach. Many problems are bluff and can be easily solved using brute force. Keep Calm**
+
+## Problem C. [QUESTION](https://codeforces.com/contest/1462/problem/C)
+
+- Simple problem. 
+- Needed to write and visualize the test cases.
+- Solved easily but took a bit of time for visualizing and typing.<br>
+Learning: **Nothing to learn. Simple logical problem**.
+
+## Problem D. [QUESTION](https://codeforces.com/contest/1462/problem/D)
+
+- Not able solve during contest. Need to upsolve.
+- 
+- 
+- 
+  <br>
+
+Learning: **Yet to upsolve problem D**
+<br><br>
+
