c01e0de5259826c4741d27f11499005c3adc9a25
Utkarsh Chaurasia <msdianuc07@gmail.com>
2021-02-03 21:03:21 +0530

    Update and rename Left-View to Left-View.cpp

--- GFG/DSA-Course/Tree/Left-View.cpp
+++ GFG/DSA-Course/Tree/Left-View.cpp
@@ -69,3 +69,32 @@ vector<int> leftView(Node *root)
    }
    return res;
 }
+
+//RECURSIVE SOLUTION
+
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right; 
+  Node(int k){
+      key=k;
+      left=right=NULL;
+  }
+};
+
+int maxLevel=0;
+void printLeft(Node *root,int level){
+    if(root==NULL)
+        return;
+    if(maxLevel<level){
+        cout<<root->key<<" ";
+        maxLevel=level;
+    }
+    printLeft(root->left,level+1);
+    printLeft(root->right,level+1);
+}
+
+void printLeftView(Node *root){
+    printLeft(root,1);
+}
