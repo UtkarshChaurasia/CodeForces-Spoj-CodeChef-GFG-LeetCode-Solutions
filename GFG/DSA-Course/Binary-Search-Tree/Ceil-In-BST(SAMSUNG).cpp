512e31798ec6048fa0c3702590b3bac34abbe7ea
Utkarsh Chaurasia <msdianuc07@gmail.com>
2021-03-01 17:53:26 +0530

    Create Ceil-In-BST(SAMSUNG).cpp

---
+++ GFG/DSA-Course/Binary-Search-Tree/Ceil-In-BST(SAMSUNG).cpp
@@ -0,0 +1,59 @@
+/*
+Given a BST and a number X, find Ceil of X.
+Note: Ceil(X) is a number that is either equal to X or is immediately greater than X.
+
+Example 1:
+
+Input:
+      5
+    /   \
+   1     7
+    \
+     2 
+      \
+       3
+X = 3
+Output: 3
+Explanation: We find 3 in BST, so ceil
+of 3 is 3.
+Example 2:
+
+Input:
+     10
+    /  \
+   5    11
+  / \ 
+ 4   7
+      \
+       8
+X = 6
+Output: 7
+Explanation: We find 7 in BST, so ceil
+of 6 is 7.
+Your task:
+You don't need to read input or print anything. Just complete the function findCeil() to implement ceil in BST which returns the ceil of X in the 
+given BST.
+
+Expected Time Complexity: O(Height of the BST)
+Expected Auxiliary Space: O(Height of the BST).
+*/
+
+int findCeil(Node* root, int input) 
+{ 
+    // Base case 
+    if (root == NULL) 
+        return -1;
+    int res = -1;
+    while(root!=NULL)
+    {
+        if(root->data==input) return input;
+        else if(root->data<input) root=root->right;
+        else
+        {
+            if(res==-1) res = root->data;
+            else res = min(res,root->data);
+            root=root->left;
+        }
+    }
+    return res;
+}
