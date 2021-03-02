b0e533288998c4651b5fad149ecd1ce835b12419
Utkarsh Chaurasia <msdianuc07@gmail.com>
2021-03-02 17:33:55 +0530

    Create LCA.cpp

---
+++ GFG/DSA-Course/Binary-Search-Tree/LCA.cpp
@@ -0,0 +1,44 @@
+/*
+Given a Binary Search Tree (with all values unique) and two node values. Find the Lowest Common Ancestors of the two nodes in the BST.
+
+Example 1:
+
+Input:
+              5
+           /    \
+         4       6
+        /         \
+       3           7
+                    \
+                     8
+n1 = 7, n2 = 8
+Output: 7
+Example 2:
+
+Input:
+     2
+   /   \
+  1     3
+n1 = 1, n2 = 3
+Output: 2
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function LCA() which takes the root Node of the BST and two integer values 
+n1 and n2 as inputs and returns the Lowest Common Ancestor of the Nodes with values n1 and n2 in the given BST. 
+
+Expected Time Complexity: O(Height of the BST).
+Expected Auxiliary Space: O(Height of the BST).
+*/
+Node* LCA(Node *root, int n1, int n2)
+{
+   //Your code here
+   if(root==NULL) return NULL;
+   if(root->data >n1 && root->data>n2)
+   {
+       return LCA(root->left,n1,n2);
+   }
+   if(root->data<n1 && root->data<n2) 
+   {
+       return LCA(root->right,n1,n2);
+   }
+   return root;
+}
