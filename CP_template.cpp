4748be87d5ab3307cd1392c10be59f917b892f62
Utkarsh Chaurasia <utkarshchaurasia@pop-os.localdomain>
2021-07-20 19:11:05 +0530

    template updated

--- CP_template.cpp
+++ CP_template.cpp
@@ -22,12 +22,14 @@ using namespace std;
 #define S second
 #define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
 
-typedef pair<int, int> pii;
+typedef pair<int, int> pi;
 typedef pair<ll, ll>   pl; 
 typedef vector<int>   vi;
 typedef vector<ll>    vl;
 typedef vector<vi>    vvi;
 typedef vector<vl>    vvl;
+typedef vector<pi>    vpi;
+typedef vector<pl>    vpl;
 typedef set<int>   si;
 typedef set<ll>   sl;
 typedef unordered_map<int, int> umi;
